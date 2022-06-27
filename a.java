379976c8297a6b1769199496d4740d27021fb29b
sunny255005 <107548368+sunny255005@users.noreply.github.com>
2022-06-27 11:12:25 +0530

    Update a.java

--- a.java
+++ a.java
@@ -4,6 +4,6 @@ class A{
 public static void main(String args[])
 {
 
-System.out.println("JAVAAA AND JENKINS-GITHUB INTEGRATIONNN");
+System.out.println("JAVAAA AND JENKINS-GITHUB INTEGRATION");
 }
 }
